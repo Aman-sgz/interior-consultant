629465a9e4842a3bd596aec5175fa06a9a929dc2
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2020-11-20 21:39:11 -0300

    ¨update¨

---
+++ script.js
@@ -0,0 +1,6 @@
+/* const iconMenu= document.getElementById('icon-menu');
+const navMenu= document.getElementById('nav-menu');
+
+iconMenu.addEventListener('click', ()=>
+    navMenu.classList.toggle('menu--show')
+); */
